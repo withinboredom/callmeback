3b02aca0f96993823fbadf852c86932c2ac4212e
withinboredom <withinboredom@11c8b26a-584f-407e-903e-7a3ab347ade0>
2009-03-02 03:13:26 +0000

    - Changelog Rev 3 Added SMTP Server support.
    - Changelog Rev 2
    Added in button functionality
    
    git-svn-id: svn://svn.code.sf.net/p/callmeback/code@6 11c8b26a-584f-407e-903e-7a3ab347ade0

---
+++ Pop3/Pop3MessageFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace Pop3
+{
+    public class Pop3MessageFilter : Pop3Message
+    {
+        public Pop3MessageFilter(long position, long size, Socket client)
+            : base(position, size, client)
+        {
+        }
+
+        public string BodyAsText
+        {
+            get
+            {
+                return Regex.Replace(base.Body, @"<(.|\n)*?>", string.Empty);
+            }
+        }
+    }
+}
