e1751b8d10b6bf368777ca045d61ac4bc1fd5bf9
withinboredom <withinboredom@11c8b26a-584f-407e-903e-7a3ab347ade0>
2009-03-02 03:10:31 +0000

    - Changelog Rev 3 Added SMTP Server support.
    - Changelog Rev 2
    Added in button functionality
    
    git-svn-id: svn://svn.code.sf.net/p/callmeback/code@5 11c8b26a-584f-407e-903e-7a3ab347ade0

---
+++ Pop3Test/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Pop3;
+using Smtp.Net;
+
+namespace Pop3Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runner program = new runner();
+
+            program.run();
+        }
+
+        class runner
+        {
+            public string stripHTML(string html)
+            {
+                return Regex.Replace(html, @"<(.|\n)*?>", string.Empty);
+            }
+
+            public void emailme()
+            {
+                EmailSender.Send("callback@rlanders.com", "landers.robert@gmail.com", "testing uno dos tres", "body body sexy sexy");
+                EmailSender.Send("callback@rlanders.com", "callback@rlanders.com", "call 2", "+19313024501");
+            }
+
+            public ArrayList getPhoneNumbers(string body)
+            {
+                ArrayList hh = new ArrayList();
+
+                string [] lis = body.Split(new char[] { '\r', '\n', ' ' });
+
+                foreach (string i in lis)
+                {
+                    if((Regex.IsMatch(i, @"^[+][0-9]\d{3}-\d{3}-\d{4}$")) || (Regex.IsMatch(i, @"^[+][0-9]\d{3}\d{3}\d{4}$")))
+                    {
+                        hh.Add(i);
+                    }
+                }
+
+                return hh;
+            }
+
+            private string removeSpaceDashes(string body)
+            {
+                return Regex.Replace(body, @"[ -]+", "");
+            }
+
+            public void run()
+            {
+                try
+                {
+                    Pop3Client client = new Pop3Client("callback", "certify588", "proxy.rlanders.com");
+                    client.OpenInbox();
+
+                    while (client.NextEmail())
+                    {
+                        Console.WriteLine("Original Body: " + client.Body);
+
+                        string htmlless = stripHTML(client.Body);
+
+                        Console.WriteLine("HTMLless Body: " + client.Body);
+
+                        ArrayList numbers = getPhoneNumbers(htmlless);
+
+                        if (numbers.Count > 0)
+                        {
+                            Console.WriteLine("Phone Numbers Harvested: ");
+                            for (int i = 0; i < numbers.Count; i++)
+                            {
+                                Console.WriteLine("\tNo. " + i.ToString() + ": " + numbers[i]);
+                            }
+                        }
+                    }
+
+                    client.CloseConnection();
+                }
+                catch (Pop3LoginException ex)
+                {
+                    Console.WriteLine("Error logging in");
+                }
+
+                emailme();
+            }
+        }
+    }
+}
